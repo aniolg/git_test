8525f3affa58760332ac1971e92d836bedaa8b28
DESKTOP-KT0LIVF\W10 <aniol_95@hotmail.com>
2018-11-27 12:52:46 +0100

    Passar per Error amb un throw quan la id<8 + Fase3

--- JS/Coets/MODEL/app.js
+++ JS/Coets/MODEL/app.js
@@ -1,5 +1,6 @@
 var Drive = /** @class */ (function () {
     function Drive(id, maxPower) {
+        this.actualPower = 0;
         this.id = id;
         this.maxPower = maxPower;
     }
@@ -8,20 +9,50 @@ var Drive = /** @class */ (function () {
 var Rocket = /** @class */ (function () {
     function Rocket(id) {
         this.drives = [];
+        this.actualVelocity = 0;
         this.id = id;
+        this.idChecker(id);
+        this.checkedId = this.idChecker(id);
         this.numDrives = 0;
     }
-    Rocket.prototype.checkedId = function (idIn) {
+    Rocket.prototype.idChecker = function (idIn) {
         if (idIn.length >= 8) {
             return idIn;
         }
         else {
-            return " ";
+            try {
+                throw new Error("La ID del coet " + idIn + " és massa curta! (introdueixi una ID de mínim 8 caràcters)");
+            }
+            catch (e) {
+                return e.message;
+            }
         }
     };
     Rocket.prototype.addDrive = function (idDrive, maxPowerDrive) {
         this.drives.push(new Drive(idDrive, maxPowerDrive));
         ++this.numDrives;
+    };
+    Rocket.prototype.accelerate = function () {
+        for (var i = 0; i < this.drives.length; i++) {
+            if (this.drives[i].actualPower < this.drives[i].maxPower) {
+                this.drives[i].actualPower += 10;
+                i = this.drives.length;
+            }
+        }
+    };
+    Rocket.prototype.slowDown = function () {
+        for (var i = this.drives.length - 1; i >= 0; i--) {
+            if (this.drives[i].actualPower > 0) {
+                this.drives[i].actualPower -= 10;
+                i = -1;
+            }
+        }
+    };
+    Rocket.prototype.getActualVelocity = function () {
+        this.actualVelocity = 0;
+        for (var i = 0; i < this.drives.length; i++) {
+            this.actualVelocity += this.drives[i].actualPower;
+        }
     };
     return Rocket;
 }());
--- JS/Coets/MODEL/app.ts
+++ JS/Coets/MODEL/app.ts
@@ -1,35 +1,74 @@
 
 class Drive {
     id: string;
+    actualPower = 0;
     maxPower: number;
+
     constructor(id: string, maxPower: number) {
         this.id = id;
         this.maxPower = maxPower;
     }
+
 }
 
 class Rocket {
     id: string;
+    checkedId: string;
     numDrives: number;
-    drives: Drive[]=[];
+    drives: Drive[] = [];
+    actualVelocity = 0;
 
     constructor(id: string) {
         this.id = id;
+        this.idChecker(id);
+        this.checkedId = this.idChecker(id);
         this.numDrives = 0;
+
     }
 
-    checkedId(idIn:string){
-        if (idIn.length >= 8){
+    idChecker(idIn: string) {
+        if (idIn.length >= 8) {
             return idIn;
-        }else{
-            return " ";
+        } else {
+            try {
+                throw new Error("La ID del coet " + idIn + " és massa curta! (introdueixi una ID de mínim 8 caràcters)");
+            } catch (e) {
+                return e.message;
+            }
         }
     }
 
-    addDrive(idDrive,maxPowerDrive) {
-            this.drives.push(new Drive(idDrive, maxPowerDrive));
-            ++this.numDrives;
+    addDrive(idDrive, maxPowerDrive) {
+        this.drives.push(new Drive(idDrive, maxPowerDrive));
+        ++this.numDrives;
+    }
+
+    accelerate() {
+        for (var i = 0; i < this.drives.length; i++) {
+            if (this.drives[i].actualPower < this.drives[i].maxPower) {
+                this.drives[i].actualPower += 10;
+                i = this.drives.length;
+            }
         }
-    
+    }
+
+    slowDown() {
+        for (var i = this.drives.length - 1; i >= 0; i--) {
+         
+            if (this.drives[i].actualPower > 0) {
+                this.drives[i].actualPower -= 10;
+                i = -1;
+            }
+            
+        }
+    }
+
+    getActualVelocity() {
+    this.actualVelocity = 0;
+        for (var i = 0; i < this.drives.length; i++) {
+            this.actualVelocity += this.drives[i].actualPower;
+        }
+    }
+
 
 }
