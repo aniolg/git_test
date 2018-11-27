8ac11ecfb8b50a967efea1899eda5118ab55b57a
DESKTOP-KT0LIVF\W10 <aniol_95@hotmail.com>
2018-11-27 12:53:16 +0100

    Fase 3 + representació final

--- JS/Coets/CONTROLLER/controller.js
+++ JS/Coets/CONTROLLER/controller.js
@@ -10,24 +10,53 @@ rockets[1].addDrive("D1_" + rockets[1].id, 50);
 rockets[1].addDrive("D1_" + rockets[1].id, 50);
 rockets[1].addDrive("D1_" + rockets[1].id, 30);
 rockets[1].addDrive("D1_" + rockets[1].id, 10);
+//accelerar 3 cops
+for (var i = 0; i < 3; i++) {
+    rockets[0].accelerate();
+    rockets[1].accelerate();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert("Velocitat coet " + rockets[0].id + ": " + rockets[0].actualVelocity + " || Velocitat coet " + rockets[1].id + ": " + rockets[1].actualVelocity);
+//frena 5 cops
+for (var i = 0; i < 5; i++) {
+    rockets[0].slowDown();
+    rockets[1].slowDown();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert("Velocitat coet " + rockets[0].id + ": " + rockets[0].actualVelocity + " || Velocitat coet " + rockets[1].id + ": " + rockets[1].actualVelocity);
+//accelerar 15 cops
+for (var i = 0; i < 15; i++) {
+    rockets[0].accelerate();
+    rockets[1].accelerate();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert("Velocitat coet " + rockets[0].id + ": " + rockets[0].actualVelocity + " || Velocitat coet " + rockets[1].id + ": " + rockets[1].actualVelocity);
 function listRockets() {
     var rocketsList = "<ul class=\"list-group\">";
     for (var i = 0; i < rockets.length; i++) {
-        if (rockets[i].checkedId(rockets[i].id) == " ") {
+        if (rockets[i].checkedId != rockets[i].id) {
             rocketsList +=
-                "<li class=\"list-group-item list-group-item-warning\">\n                <i class=\"fas fa-exclamation-triangle\"></i> \n                La ID del coet \u00E9s massa curta! (introdueixi una ID de m\u00EDnim 8 car\u00E0cters)\n                </li>";
+                "<li class=\"list-group-item list-group-item-warning\">\n                <i class=\"fas fa-exclamation-triangle\"></i> \n                " + rockets[i].checkedId + "\n                </li>";
         }
         else {
             rocketsList +=
-                "<li class=\"list-group-item\">\n                <i class=\"fas fa-space-shuttle fa-rotate-270\"></i> \n                El coet " + rockets[i].id + " te " + rockets[i].numDrives + " propulsors:";
+                "<li class=\"list-group-item\">\n                <p><i class=\"fas fa-space-shuttle fa-rotate-270\"></i> \n                El coet " + rockets[i].id + " te " + rockets[i].numDrives + " propulsors:</p>";
             for (var j = 0; j < rockets[i].drives.length; j++) {
-                if (j != rockets[i].drives.length - 1) {
-                    rocketsList += " " + rockets[i].drives[j].maxPower + ",";
+                if (rockets[i].drives[j].actualPower == rockets[i].drives[j].maxPower) {
+                    rocketsList += " \n                        <p class=\"badge badge-secondary\">\n                        <i class=\"fas fa-battery-full\"></i>\n                        Pact: " + rockets[i].drives[j].actualPower + " (Pmax:" + rockets[i].drives[j].maxPower + ")</p>\n                        ";
+                }
+                else if (rockets[i].drives[j].actualPower == 0) {
+                    rocketsList += " \n                        <p class=\"badge badge-secondary\">\n                        <i class=\"fas fa-battery-empty\"></i>\n                        Pact: " + rockets[i].drives[j].actualPower + " (Pmax:" + rockets[i].drives[j].maxPower + ")</p>\n                        ";
                 }
                 else {
-                    rocketsList += " " + rockets[i].drives[j].maxPower + "</li>";
+                    rocketsList += " \n                        <p class=\"badge badge-secondary\">\n                        <i class=\"fas fa-battery-half\"></i>\n                        Pact: " + rockets[i].drives[j].actualPower + " (Pmax:" + rockets[i].drives[j].maxPower + ")</p>\n                        ";
                 }
             }
+            rockets[i].getActualVelocity();
+            rocketsList += "\n                <span class=\"badge badge-pill badge-primary\">\n                <i class=\"fas fa-stopwatch\"></i>\n                Velocitat: " + rockets[i].actualVelocity + "\n                </span></li>";
         }
     }
     rocketsList += "</ul>";
--- JS/Coets/CONTROLLER/controller.ts
+++ JS/Coets/CONTROLLER/controller.ts
@@ -12,30 +12,79 @@ rockets[1].addDrive("D1_" + rockets[1].id, 50);
 rockets[1].addDrive("D1_" + rockets[1].id, 30);
 rockets[1].addDrive("D1_" + rockets[1].id, 10);
 
+//accelerar 3 cops
+for (var i = 0; i < 3; i++){
+    rockets[0].accelerate();
+    rockets[1].accelerate();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert (`Velocitat coet ${rockets[0].id}: ${rockets[0].actualVelocity} || Velocitat coet ${rockets[1].id}: ${rockets[1].actualVelocity}`);
+
+//frena 5 cops
+for (var i = 0; i < 5; i++){
+    rockets[0].slowDown();
+    rockets[1].slowDown();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert (`Velocitat coet ${rockets[0].id}: ${rockets[0].actualVelocity} || Velocitat coet ${rockets[1].id}: ${rockets[1].actualVelocity}`);
+
+//accelerar 15 cops
+for (var i = 0; i < 15; i++){
+    rockets[0].accelerate();
+    rockets[1].accelerate();
+}
+rockets[0].getActualVelocity();
+rockets[1].getActualVelocity();
+alert (`Velocitat coet ${rockets[0].id}: ${rockets[0].actualVelocity} || Velocitat coet ${rockets[1].id}: ${rockets[1].actualVelocity}`);
+
 function listRockets() {
     var rocketsList = `<ul class="list-group">`;
 
     for (var i = 0; i < rockets.length; i++) {
-        if (rockets[i].checkedId(rockets[i].id) == " ") {
+        if (rockets[i].checkedId != rockets[i].id) {
             rocketsList +=
                 `<li class="list-group-item list-group-item-warning">
                 <i class="fas fa-exclamation-triangle"></i> 
-                La ID del coet és massa curta! (introdueixi una ID de mínim 8 caràcters)
+                ${rockets[i].checkedId}
                 </li>`
         } else {
             rocketsList +=
                 `<li class="list-group-item">
-                <i class="fas fa-space-shuttle fa-rotate-270"></i> 
-                El coet ${rockets[i].id} te ${rockets[i].numDrives} propulsors:`;
+                <p><i class="fas fa-space-shuttle fa-rotate-270"></i> 
+                El coet ${rockets[i].id} te ${rockets[i].numDrives} propulsors:</p>`;
 
             for (var j = 0; j < rockets[i].drives.length; j++) {
 
-                if (j != rockets[i].drives.length - 1) {
-                    rocketsList += ` ${rockets[i].drives[j].maxPower},`;
+
+                if (rockets[i].drives[j].actualPower == rockets[i].drives[j].maxPower) {
+                    rocketsList += ` 
+                        <p class="badge badge-secondary">
+                        <i class="fas fa-battery-full"></i>
+                        Pact: ${rockets[i].drives[j].actualPower} (Pmax:${rockets[i].drives[j].maxPower})</p>
+                        `;
+                } else if (rockets[i].drives[j].actualPower == 0) {
+                    rocketsList += ` 
+                        <p class="badge badge-secondary">
+                        <i class="fas fa-battery-empty"></i>
+                        Pact: ${rockets[i].drives[j].actualPower} (Pmax:${rockets[i].drives[j].maxPower})</p>
+                        `;
                 } else {
-                    rocketsList += ` ${rockets[i].drives[j].maxPower}</li>`;
+                    rocketsList += ` 
+                        <p class="badge badge-secondary">
+                        <i class="fas fa-battery-half"></i>
+                        Pact: ${rockets[i].drives[j].actualPower} (Pmax:${rockets[i].drives[j].maxPower})</p>
+                        `;
                 }
             }
+
+            rockets[i].getActualVelocity();
+            rocketsList += `
+                <span class="badge badge-pill badge-primary">
+                <i class="fas fa-stopwatch"></i>
+                Velocitat: ${rockets[i].actualVelocity}
+                </span></li>`
         }
     }
 
@@ -47,4 +96,5 @@ function listRockets() {
 function loadContent() {
     document.getElementById("div_coets").innerHTML = listRockets();
 }
+
 
